205867371296ba3405821e03a593d6dd8f833b57
best-lucky1030 <bestlucky1030@gmail.com>
2022-06-10 06:37:16 +0300

    update app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,54 @@
 <template>
-  <Navbar />
+  <v-app>
+    <Navbar @searchProducts = "searchProducts" :cart="cart" :subtotalCart="subtotalCart" />
+    <v-contene>
+      <router-view :products="filteredProducts" @addCart = "addCart"></router-view>
+    </v-contene>
+    <Footer v-if="pageLoaded" />
+  </v-app>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
 
 export default {
   name: 'App',
   data: () => {
-
+    return {
+      pageLoaded: false,
+      products: null,
+      searchQuery: "",
+      cart: []
+    };
   },
   components: {
-    Navbar
+    Navbar,
+    Footer
+  },
+  mounted: function() {
+    this.cart = localStorage.getItem("cart")
+      ? JSON.parse(localStorage.getItem("cart"))
+      : [];
+
+    axios.get("https://hplussport.com/api/products/order/price").then(res => {
+      this.products = res.data;
+    });
+
+    setTimeout(() => {
+      tihs.pageLoaded - true
+    }, 2000);
+  },
+  methods: {
+    searchproducts: function(query) {
+      this.searchQuery = query;
+    },
+    addCart: function(products) {
+    this.cart = products;
+    localStorage.setItem("cart", JSON.stringify(products));
+    }
   }
-}
+};
 </script>
 
 <style scoped>
---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Footer</h1>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  data: () => {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
